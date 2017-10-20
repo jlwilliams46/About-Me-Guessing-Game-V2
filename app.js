f4feb6de43f79677297c228df5a1d5168327ef3e
jlwilliams46 <jwilliams492@gmail.com>
2017-10-20 02:13:10 -0700

    resubmitted project due to error in file, originally turned on 10/19 at 2:55am

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var correctcounters = 0
+var correctAnswers = 0;
 // introducion
 var user = prompt('Welcome! What is your name?');
 console.log('User:', user);
@@ -12,7 +12,7 @@ console.log('Jamie\'s favorite season:', season);
 // validation...response if answer is correct
 if(season.toLowerCase() === 'y') {
   alert('Correct!');
-  correctcounters++;
+  correctAnswers++;
 
 // validation...response if answer is incorrect
 }else{
@@ -26,7 +26,8 @@ console.log('Can Jamie swim:', swim);
 // validation...response if answer is correct
 if(swim.toLowerCase() === 'n') {
   alert('Correct!');
-  correctcounters++;
+  correctAnswers++;
+
 // validation...response if answer is incorrect
 }else{
   alert('Incorrect!');
@@ -39,7 +40,8 @@ console.log('Has Jamie seen Seether live:', seether);
 // validation...response if answer is correct
 if(seether.toLowerCase() === 'y') {
   alert('Correct!');
-  correctcounters++;
+  correctAnswers++;
+
 // validation...response if answer is incorrect
 }else{
   alert('Incorrect!');
@@ -52,7 +54,8 @@ console.log('Does Jamie own a cat:', cat);
 // validation...response if answer is correct
 if(cat.toLowerCase() === 'n') {
   alert('Correct!');
-  correctcounters++;
+  correctAnswers++;
+
 // validation...response if answer is incorrect
 }else{
   alert('Incorrect!');
@@ -65,7 +68,8 @@ console.log('Can Jamie Sing?:', sing);
 // validation...response if answer is correct
 if(sing.toLowerCase() === 'y') {
   alert('Correct!');
-  correctcounters++;
+  correctAnswers++;
+
 // validation...response if answer is incorrect
 }else{
   alert('Incorrect!');
@@ -75,12 +79,37 @@ if(sing.toLowerCase() === 'y') {
 var pokemon = prompt('How many Pokémon did Jamie catch yesterday?\n(Please enter a number)');
 console.log('Number of Pokémon:', pokemon);
 
-while (pokemon !== 7) {  //Found your issue Jamie. You have an infinite loop <3
+while (parseInt(pokemon) !== 7) {
   alert('Incorrect! Try Again');
   pokemon = prompt('How many Pokémon did Jamie catch yesterday?\n(Please enter a number)');
   console.log('Number of Pokémon:', pokemon);
 }
 alert('Correct!');
-correctcounters++;
+correctAnswers++;
 
-alert('you got ' +   correctcounters + ' out of 7');
+// seventh question'
+var states = ['Indiana', 'Georgia', 'North Carolina', 'Maryland', 'Virgina', 'Missouri', 'Alabama', 'Kentucky'];
+var guess = prompt('Guess which states I\'ve lived in other than Washington.\n(You have maximum of 6 tries.)');
+var flag;
+
+
+for (var i = 0; i <= 4; i++) {
+  console.log('States Guessed:', guess);
+
+  if(guess !== states[i]) {
+    prompt('Incorrect! Guess which states I\'ve lived in other than Washington.');
+  }
+
+  if(guess === states[i]) {
+    alert('Correct! Jamie has lived in the following states: ' + states[0] + ', ' + states[1] + ', ' + states[2] + ', ' + states[3] + ', ' + states[4]) + ', ' + states[5] + ', ' + states[6]; + ', and ' + states[7] + '.';
+    flag = true; // set flag equal to true
+    correctAnswers++;
+    break; // exits loop once condition is met
+  }
+}
+
+// only runs if false
+if(!flag) {
+  alert('You are out of guesses! Jamie has lived in the following states: ' + states);
+}
+alert('You got ' + correctAnswers + ' out of 7!');
